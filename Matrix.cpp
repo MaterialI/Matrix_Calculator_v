6979d6f2d9722f14581898eb6443887f54559ba6
MaterialI <94946672+MaterialI@users.noreply.github.com>
2022-12-24 10:10:21 -0800

    inverse

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,240 +1,10 @@
-//#include "Matrix.h"
-//#include <iostream>
-//using namespace std;
-////default constructor with redundant features
-//Matrix::Matrix()
-//{
-//	sizeX = 1;								
-//	sizeY = 1;								
-//	arr = new float* [sizeY];		
-//	for (int i = 0; i < sizeY; i++)
-//	{
-//		arr[i] = new float[sizeX];
-//		for (int j = 0; j < sizeX; j++)
-//			arr[i][j] = 0;
-//	}
-//}
-//Matrix::Matrix(int size)
-//{
-//	sizeX = sizeY = size;
-//	arr = new float* [size];
-//	for(int i = 0; i<size; i++)
-//	{
-//		arr[i] = new float[size];
-//		for(int j= 0;j<size;j++)
-//		{
-//			if(i == j)
-//			{
-//				arr[i][j] = 1;
-//			}
-//			else arr[i][j] = 0;
-//		}
-//	}
-//}
-////overloaded constructor
-//Matrix::Matrix(int sizeY, int sizeX)
-//{
-//	this->sizeY = sizeY;
-//	this->sizeX = sizeX;
-//	arr = new float* [sizeY];
-//	for (int i = 0; i < sizeY; i++)
-//	{
-//		arr[i] = new float[sizeX];
-//		for (int j = 0; j < sizeX; j++)
-//			arr[i][j] = 0;
-//	}
-//}
-////copy constructor
-//Matrix::Matrix(const Matrix& other)
-//{
-//	sizeX = other.sizeX;
-//	sizeY = other.sizeY;
-//	arr = new float* [sizeY]; //operator* doesnt pass matrix tp other;
-//	for (int i = 0; i < sizeY; i++)
-//	{
-//		arr[i] = new float[sizeX];
-//		for (int j = 0; j < sizeX; j++)
-//			arr[i][j] = other.arr[i][j];
-//	}
-//}
-//Matrix::Matrix(float** p_arr, int sizeY, int sizeX)
-//{
-//	this->sizeX = sizeX;
-//	this->sizeY = sizeY;
-//	arr = new float* [sizeY];
-//	for (int i = 0; i < sizeY; i++)
-//	{
-//		arr[i] = new float[sizeX];
-//		for (int j = 0; j < sizeX; j++)
-//			arr[i][j] = p_arr[i][j];
-//	}
-//}
-////assignment operator
-//Matrix& Matrix::operator= (const Matrix& other)
-//{
-//	sizeX = other.sizeX;
-//	sizeY = other.sizeY;
-//	if (sizeX != other.sizeX || sizeY != other.sizeY)
-//	{
-//		
-//		delete[] arr;
-//		arr = new float* [sizeY];
-//		for (int i = 0; i < sizeY; i++)
-//		{
-//			arr[i] = new float[sizeX];
-//			for (int j = 0; j < sizeX; j++)
-//				arr[i][j] = other.arr[i][j];
-//		}
-//	}
-//	else
-//	{
-//		for (int i = 0; i < sizeY; i++)
-//		{
-//			for (int j = 0; j < sizeX; j++)
-//				arr[i][j] = other.arr[i][j];
-//		}
-//	}
-//	return *this;
-//}
-////destructor
-//Matrix::~Matrix()
-//{
-//	delete[] arr;
-//}
-////fill matrix with numbers
-//void Matrix::fill()
-//{
-//	float tmp;
-//	cout << "Please, enter your matrix sequentially: ";
-//	for (int i = 0; i < sizeY; i++)
-//	{
-//		cout << "Enter " << i << " row:";
-//		for (int j = 0; j < sizeX; j++)
-//		{
-//			cout << "Enter " << j << " entry: ";
-//			cin >> tmp;
-//			arr[i][j] = tmp;
-//		}
-//	}
-//}
-////and also this
-//void Matrix::get_matrix() const
-//{
-//	for (int i = 0; i < sizeY; i++)
-//	{
-//		cout << "[ ";
-//		for (int j = 0; j < sizeX; j++)
-//			cout << arr[i][j] << " ";
-//		
-//		cout <<" ]" << '\n';
-//	}
-//}
-////be careful  (empties matrix), soon complementing and merging will be added. 
-//void Matrix::reset_size(int n_sizeY, int n_sizeX)
-//{
-//	if (sizeX != n_sizeX || sizeY != n_sizeY)
-//	{
-//		delete arr;
-//		sizeX = n_sizeX;
-//		sizeY = n_sizeY;
-//		arr = new float* [sizeY];
-//		for (int i = 0; i < sizeY; i++)
-//		{
-//			arr[i] = new float[sizeX];
-//			for (int j = 0; j < sizeX; j++)
-//				arr[i][j] = 0;
-//		}
-//	}
-//}
-///////operations
-//Matrix Matrix::operator+(const Matrix& other)
-//{
-//	Matrix nm(*this);
-//	if (sizeY == other.sizeY && sizeX == other.sizeX)
-//	{
-//		for (int i = 0; i < sizeY; i++)
-//		{
-//			for (int j = 0; j < sizeX; j++)
-//				nm.arr[i][j] += other.arr[i][j];
-//		}
-//	}
-//	else
-//		cout << "Addition can't be performed: different dimensions"<<endl;
-//	return nm;
-//}
-//Matrix Matrix::operator-(const Matrix& other)
-//{
-//	Matrix nm(*this);
-//	if (sizeY == other.sizeY && sizeX == other.sizeX)
-//	{
-//		
-//		for (int i = 0; i < sizeY; i++)
-//		{
-//			for (int j = 0; j < sizeX; j++)
-//				nm.arr[i][j] -= other.arr[i][j];
-//		}
-//	}
-//	else
-//		cout << "Substraction can't be performed: different dimensions" << endl;
-//	return nm;
-//}
-//Matrix Matrix::operator*(const Matrix& other) 
-//{
-//	if (sizeX == other.sizeY/* && sizeX == sizeY*/)
-//	{
-//
-//		//if(size)
-//		Matrix res(sizeY, other.sizeX);
-//			for (int i = 0; i < other.sizeX * sizeY; i++)
-//			{
-//				for (int j = 0; j < sizeX; j++)
-//				{
-//
-//					res.arr[(i - (i % other.sizeX)) / other.sizeX][i % other.sizeX] +=
-//						arr[((i - (i % other.sizeX)) / other.sizeX)][j] * other.arr[j][(i % other.sizeX)];
-//				}
-//			}
-//		return res;
-//	}
-//	else
-//		cout << "Multiplication cannot be performed, \n please verify that you entered correct dimentions for matricies" << endl;
-//}
-//Matrix Matrix::operator* (float cf)
-//{
-//	for (int i = 0; i < sizeY; i++)
-//		for (int j = 0; j < sizeY; j++)
-//			arr[i][j] *= cf;
-//	return *this;
-//}
-//bool Matrix::operator==(const Matrix& other) const
-//{
-//	bool equal = true; 
-//	if(this->sizeX!= other.sizeX|| this->sizeY!= other.sizeY)
-//	{
-//		return false;
-//	}
-//	for(int i = 0 ; i< sizeY;i++)
-//	{}
-//}
-//Matrix Matrix::invese()
-//{
-//	Matrix id(this->sizeX);
-//	Matrix id_ref(sizeX);
-//	
-//}
-//Matrix& rec_inv(Matrix& other, Matrix& id, const Matrix& id_ref)
-//{
-//	if(id_ref == other)
-//		return other;
-//	else
-//	{
-//		
-//	}
-//}
+
 #include "Matrix.h"
 #include <iostream>
 using namespace std;
 //default constructor with redundant features
+//description: populates the matrix of size 1x1 with value 0
+
 Matrix::Matrix()
 {
 	sizeX = 1;
@@ -247,6 +17,10 @@ Matrix::Matrix()
 			arr[i][j] = 0;
 	}
 }
+
+//overloaded constructor
+//description: creates square matrix based on size argument
+//postcondition: identity matrix is created
 Matrix::Matrix(int size)
 {
 	sizeX = size;
@@ -266,7 +40,10 @@ Matrix::Matrix(int size)
 		}
 	}
 }
+
 //overloaded constructor
+//description: creates any matrix including vectors based on the arguments of size x and y. 
+//postcondition: matrix/vector created and populated with 0s
 Matrix::Matrix(int sizeY, int sizeX)
 {
 	this->sizeY = sizeY;
@@ -280,7 +57,10 @@ Matrix::Matrix(int sizeY, int sizeX)
 			arr[i][j] = 0;
 	}
 }
+
 //copy constructor
+//description: instantiates a copy of given matrix
+//postcondition: the matrix instance is created
 Matrix::Matrix(const Matrix& other)
 {
 	sizeX = other.sizeX;
@@ -293,6 +73,11 @@ Matrix::Matrix(const Matrix& other)
 			arr[i][j] = other.arr[i][j];
 	}
 }
+
+//overloaded constructor 
+//description: instantiates an instance of a matrix based on a 2-dimentional 
+// float** array and size x and y of the array
+//postcondition: 
 Matrix::Matrix(float** p_arr, int sizeY, int sizeX)
 {
 	this->sizeX = sizeX;
